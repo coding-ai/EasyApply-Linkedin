88f743c95293577f0f0f73874ea3e74d646985af
willzhengwang <zhengwang615@outlook.com>
2024-01-09 00:49:52 -0800

    WIP-get all job links on a page

--- README.md
+++ README.md
@@ -14,7 +14,9 @@ These instructions will get you a copy of the project up and running on your loc
 
 2. Selenium requires a driver to interface with the chosen browser. Make sure the driver is in your path, you will need to add your `driver_path` to the `config.json` file.
 
-I used the Chrome driver, you can download it [here](https://sites.google.com/a/chromium.org/chromedriver/downloads). You can also download [Edge](https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/), [Firefox](https://github.com/mozilla/geckodriver/releases) or [Safari](https://webkit.org/blog/6900/webdriver-support-in-safari-10/). Depends on your preferred browser.
+I used the Chrome driver, you can download it [here](https://chromedriver.chromium.org/downloads). You can also download [Edge](https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/), [Firefox](https://github.com/mozilla/geckodriver/releases) or [Safari](https://webkit.org/blog/6900/webdriver-support-in-safari-10/). Depends on your preferred browser.
+
+Click [here](https://selenium-python.readthedocs.io/locating-elements.html) to check out a very useful document for using Selenium.
 
 ### Usage
 
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class EasyApplyLinkedin:
         self.password = data['password']
         self.keywords = data['keywords']
         self.location = data['location']
-        self.driver = webdriver.Chrome(data['driver_path'])
+        self.driver = webdriver.Chrome(data["driver_path"])
 
     def login_linkedin(self):
         """This function logs into your personal LinkedIn profile"""
@@ -27,10 +27,10 @@ class EasyApplyLinkedin:
         self.driver.get("https://www.linkedin.com/login")
 
         # introduce email and password and hit enter
-        login_email = self.driver.find_element_by_name('session_key')
+        login_email = self.driver.find_element('name', 'session_key')
         login_email.clear()
         login_email.send_keys(self.email)
-        login_pass = self.driver.find_element_by_name('session_password')
+        login_pass = self.driver.find_element('name', 'session_password')
         login_pass.clear()
         login_pass.send_keys(self.password)
         login_pass.send_keys(Keys.RETURN)
@@ -39,115 +39,138 @@ class EasyApplyLinkedin:
         """This function goes to the 'Jobs' section a looks for all the jobs that matches the keywords and location"""
 
         # go to Jobs
-        jobs_link = self.driver.find_element_by_link_text('Jobs')
+        print("Search jobs by title and location.")
+        jobs_link = self.driver.find_element(By.LINK_TEXT, 'Jobs')
         jobs_link.click()
 
         # search based on keywords and location and hit enter
-        search_keywords = self.driver.find_element_by_css_selector(".jobs-search-box__text-input[aria-label='Search jobs']")
+        search_keywords = self.driver.find_element(By.XPATH, '//input[starts-with(@id, "jobs-search-box-keyword-id-ember")]')
         search_keywords.clear()
         search_keywords.send_keys(self.keywords)
-        search_location = self.driver.find_element_by_css_selector(".jobs-search-box__text-input[aria-label='Search location']")
+        search_keywords.send_keys(Keys.RETURN)
+
+        search_location = self.driver.find_element(By.XPATH, '//input[starts-with(@id, "jobs-search-box-location-id-ember")]')
         search_location.clear()
         search_location.send_keys(self.location)
         search_location.send_keys(Keys.RETURN)
+        time.sleep(5)
+
+        print("Filter the result by select jobs posted in the past 24 hours")
+        date_posted_button = self.driver.find_element(By.XPATH,
+                                                 '//button[starts-with(@aria-label, "Date posted filter.")][@type="button"]')
+
+        date_posted_button.click()
+        time.sleep(1)
+
+        past24h_radiobutton = self.driver.find_element(By.XPATH, "//span[text()='Past 24 hours']")
+        past24h_radiobutton.click()
+        time.sleep(1)
+
+        show_results_button = self.driver.find_element(By.XPATH,
+                                                  "//span[contains(normalize-space(), 'Show') and contains(normalize-space(), 'results')]")
+        show_results_button.click()
+        time.sleep(1)
+
+        # collect all job urls on this page
+
 
     def filter(self):
-        """This function filters all the job results by 'Easy Apply'"""
+        """This function filters all the job results by 'Date Posted' = Past 24 hours """
 
-        # select all filters, click on Easy Apply and apply the filter
-        all_filters_button = self.driver.find_element_by_xpath("//button[@data-control-name='all_filters']")
-        all_filters_button.click()
+        print("Filter the result by select jobs posted in the past 24 hours")
+        date_posted_button = self.driver.find_element(By.XPATH,
+                                                 '//button[starts-with(@aria-label, "Date posted filter.")][@type="button"]')
+
+        date_posted_button.click()
         time.sleep(1)
-        easy_apply_button = self.driver.find_element_by_xpath("//label[@for='f_LF-f_AL']")
-        easy_apply_button.click()
+
+        past24h_radiobutton = self.driver.find_element(By.XPATH, "//span[text()='Past 24 hours']")
+        past24h_radiobutton.click()
         time.sleep(1)
-        apply_filter_button = self.driver.find_element_by_xpath("//button[@data-control-name='all_filters_apply']")
-        apply_filter_button.click()
+
+        show_results_button = self.driver.find_element(By.XPATH,
+                                                  "//span[contains(normalize-space(), 'Show') and contains(normalize-space(), 'results')]")
+        show_results_button.click()
+        time.sleep(1)
 
     def find_offers(self):
+        from collections import OrderedDict
+        from bs4 import BeautifulSoup
+
         """This function finds all the offers through all the pages result of the search and filter"""
 
-        # find the total amount of results (if the results are above 24-more than one page-, we will scroll trhough all available pages)
-        total_results = self.driver.find_element_by_class_name("display-flex.t-12.t-black--light.t-normal")
-        total_results_int = int(total_results.text.split(' ',1)[0].replace(",",""))
-        print(total_results_int)
+        # How to get all page buttons?
+        page_buttons = self.driver.find_elements(By.XPATH, '//button[starts-with(@aria-label, "Page ")][@type="button"]')
+        page_buttons[0].click()
+        self.driver.get(self.driver.current_url)
 
+        # Loop through each element
+        for button in page_buttons:
+            # Perform actions on each element
+            print("Button text:", button.text)
+            # button.click()  # or perform any other actions
+
+        # how to get all job links in the current page?
+        job_links = OrderedDict()  # {link: element}
+        for i in range(10):
+            all_links = self.driver.find_elements(By.XPATH, "//a")
+            for a in all_links:
+                if str(a.get_attribute('href')).startswith("https://www.linkedin.com/jobs/view"):
+                    simple_link = a.get_attribute('href').split('?')[0]
+                    if simple_link not in job_links:
+                        job_links[simple_link] = a
+            # scroll down for each job element
+            self.driver.execute_script("arguments[0].scrollIntoView();", a)
+            time.sleep(0.5)
+
+        # How to get tile, location, and description from a href?
+        job_element = job_links['https://www.linkedin.com/jobs/view/3800277563/']
+        job_element.click()
         time.sleep(2)
-        # get results for the first page
-        current_page = self.driver.current_url
-        results = self.driver.find_elements_by_class_name("occludable-update.artdeco-list__item--offset-4.artdeco-list__item.p0.ember-view")
 
-        # for each job add, submits application if no questions asked
-        for result in results:
-            hover = ActionChains(self.driver).move_to_element(result)
-            hover.perform()
-            titles = result.find_elements_by_class_name('job-card-search__title.artdeco-entity-lockup__title.ember-view')
-            for title in titles:
-                self.submit_apply(title)
+        # Find the general information of the job offers
+        self.driver.page_source
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
 
-        # if there is more than one page, find the pages and apply to the results of each page
-        if total_results_int > 24:
-            time.sleep(2)
+        job_postings = soup.find_all('li', {'class': 'jobs-search-results__list-item'})
 
-            # find the last page and construct url of each page based on the total amount of pages
-            find_pages = self.driver.find_elements_by_class_name("artdeco-pagination__indicator.artdeco-pagination__indicator--number")
-            total_pages = find_pages[len(find_pages)-1].text
-            total_pages_int = int(re.sub(r"[^\d.]", "", total_pages))
-            get_last_page = self.driver.find_element_by_xpath("//button[@aria-label='Page "+str(total_pages_int)+"']")
-            get_last_page.send_keys(Keys.RETURN)
-            time.sleep(2)
-            last_page = self.driver.current_url
-            total_jobs = int(last_page.split('start=',1)[1])
-
-            # go through all available pages and job offers and apply
-            for page_number in range(25,total_jobs+25,25):
-                self.driver.get(current_page+'&start='+str(page_number))
-                time.sleep(2)
-                results_ext = self.driver.find_elements_by_class_name("occludable-update.artdeco-list__item--offset-4.artdeco-list__item.p0.ember-view")
-                for result_ext in results_ext:
-                    hover_ext = ActionChains(self.driver).move_to_element(result_ext)
-                    hover_ext.perform()
-                    titles_ext = result_ext.find_elements_by_class_name('job-card-search__title.artdeco-entity-lockup__title.ember-view')
-                    for title_ext in titles_ext:
-                        self.submit_apply(title_ext)
-        else:
-            self.close_session()
-
-    def submit_apply(self,job_add):
-        """This function submits the application for the job add found"""
-
-        print('You are applying to the position of: ', job_add.text)
-        job_add.click()
-        time.sleep(2)
-        
-        # click on the easy apply button, skip if already applied to the position
-        try:
-            in_apply = self.driver.find_element_by_xpath("//button[@data-control-name='jobdetails_topcard_inapply']")
-            in_apply.click()
-        except NoSuchElementException:
-            print('You already applied to this job, go to next...')
-            pass
-        time.sleep(1)
-
-        # try to submit if submit application is available...
-        try:
-            submit = self.driver.find_element_by_xpath("//button[@data-control-name='submit_unify']")
-            submit.send_keys(Keys.RETURN)
-        
-        # ... if not available, discard application and go to next
-        except NoSuchElementException:
-            print('Not direct application, going to next...')
+        # Extract relevant information from each job posting and store it in a list of dictionaries
+        data = []
+        for job_posting in job_postings[:3]:
             try:
-                discard = self.driver.find_element_by_xpath("//button[@data-test-modal-close-btn]")
-                discard.send_keys(Keys.RETURN)
-                time.sleep(1)
-                discard_confirm = self.driver.find_element_by_xpath("//button[@data-test-dialog-primary-btn]")
-                discard_confirm.send_keys(Keys.RETURN)
-                time.sleep(1)
-            except NoSuchElementException:
-                pass
+                job_title = job_posting.find('a', class_='job-card-list__title').get_text().strip()
+            except AttributeError:
+                job_title = None
 
-        time.sleep(1)
+            try:
+                company_name = job_posting.find('a', class_='job-card-container__link').get_text().strip()
+            except AttributeError:
+                company_name = None
+
+            try:
+                location = job_posting.find('li', class_='job-card-container__metadata-item').get_text().strip()
+            except AttributeError:
+                location = None
+
+            job_description_element = job_posting.find('div', class_='jobs-description-content__text')
+
+            # job_description_element = self.driver.find_element(By.XPATH, "//div[text()='About the job']")
+            #
+            # contains(normalize - space(), 'Show')
+            "About the job"
+            # Extract the text content of the job description element
+            job_description = job_description_element.get_text(
+                strip=True) if job_description_element else "Job description not found."
+            print(job_description)
+
+            time.sleep(0.5)
+
+            data.append({
+                'Job Title': job_title,
+                'Company Name': company_name,
+                'Location': location,
+                'Description': job_description
+            })
 
     def close_session(self):
         """This function closes the actual session"""
@@ -162,7 +185,7 @@ class EasyApplyLinkedin:
         self.login_linkedin()
         time.sleep(5)
         self.job_search()
-        time.sleep(5)
+        time.sleep(2)
         self.filter()
         time.sleep(2)
         self.find_offers()
@@ -171,8 +194,7 @@ class EasyApplyLinkedin:
 
 
 if __name__ == '__main__':
-
-    with open('config.json') as config_file:
+    with open('data/config.json') as config_file:
         data = json.load(config_file)
 
     bot = EasyApplyLinkedin(data)
