44f2e6cfd7c71f0e81960658018b32ca5619702e
willzhengwang <zhengwang615@outlook.com>
2024-01-10 09:50:39 -0800

    added config_log function

--- main.py
+++ main.py
@@ -1,44 +1,51 @@
 #!/usr/bin/env python
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
-import time
-import re
 import json
+import time
 from collections import OrderedDict, deque
-from bs4 import BeautifulSoup
-import pandas as pd
 from datetime import datetime
-from os import path
+from os import path, makedirs
 from random import randint
+import logging
+
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class SearchLinkedin:
 
     def __init__(self, config):
         """Parameter initialization"""
-
         self.email = config['email']
         self.password = config['password']
         self.keywords = config['keywords']
         self.location = config['location']
+        self.driver_path = config["driver_path"]
 
-        self.driver = webdriver.Chrome(config["driver_path"])
-        self.driver.implicitly_wait(10)  # seconds
-
+        self.driver = None
         self.job_data = {'Link': [], 'Title': [], 'Company': [], 'Location': [], 'Description': []}
+
+    def init_webdriver(self):
+        """
+        Initialize a Chrome webdriver.
+        """
+        logging.info(f"Initialize a Chrome driver.")
+        self.driver = webdriver.Chrome(self.driver_path)
+        self.driver.implicitly_wait(10)  # seconds
+        time.sleep(0.5)
+        self.driver.maximize_window()
+        time.sleep(0.5)
 
     def login_linkedin(self):
         """This function logs into your personal LinkedIn profile"""
 
-        # go to the LinkedIn login url
+        logging.info(f"Go to the LinkedIn login url: https://www.linkedin.com/login.")
         self.driver.get("https://www.linkedin.com/login")
 
         # introduce email and password and hit enter
+        logging.info(f"Fill email and password and hit enter.")
         login_email = self.driver.find_element('name', 'session_key')
         login_email.clear()
         login_email.send_keys(self.email)
@@ -52,7 +59,7 @@ class SearchLinkedin:
         """This function goes to the 'Jobs' section a looks for all the jobs that matches the keywords and location"""
 
         # go to Jobs
-        print("Search jobs by title and location.")
+        logging.info("Search jobs by title and location.")
         jobs_link = self.driver.find_element(By.LINK_TEXT, 'Jobs')
         jobs_link.click()
         time.sleep(randint(3, 6))
@@ -61,7 +68,9 @@ class SearchLinkedin:
         while tries < 10:
             try:
                 # search based on keywords and location and hit enter
-                print(f"Number of trial {tries}. Search {self.keywords} at {self.location}")
+                if tries > 1:
+                    logging.info(f"Number of trials for searching jobs: {tries}. ")
+                logging.info(f"Search {self.keywords} at {self.location}")
                 search_keywords = self.driver.find_element(By.XPATH, '//input[starts-with(@id, "jobs-search-box-keyword-id-ember")]')
                 search_keywords.clear()
                 search_keywords.click()
@@ -79,17 +88,17 @@ class SearchLinkedin:
                 time.sleep(randint(3, 6))
                 search_location.send_keys(Keys.RETURN)
                 time.sleep(randint(5, 8))
-                print("Job search is done!")
+                logging.info("Job search is done!")
                 break
             except Exception as e:
-                print(str(e))
+                logging.error(str(e))
                 time.sleep(randint(3, 6))
             tries += 1
 
     def filter(self):
         """This function filters all the job results by 'Date Posted' = Past 24 hours """
 
-        print("Filter the result by select jobs posted in the past 24 hours")
+        logging.info("Select jobs posted in the past 24 hours")
         tries = 1
         while tries < 10:
             try:
@@ -104,10 +113,10 @@ class SearchLinkedin:
                 show_results_button = self.driver.find_element(By.XPATH,"//span[contains(normalize-space(), 'Show') and contains(normalize-space(), 'results')]")
                 show_results_button.click()
                 time.sleep(randint(4, 8))
-                print("Job filtering is done!")
+                logging.info("Job filtering is done!")
                 break
             except Exception as e:
-                print(str(e))
+                logging.error(str(e))
                 time.sleep(randint(3, 6))
             tries += 1
 
@@ -165,7 +174,7 @@ class SearchLinkedin:
         # Extract relevant information from each job posting and store it in a list of dictionaries
         data = {'Link': [], 'Title': [], 'Company': [], 'Location': [], 'Description': []}
         for i, (link, job_element) in enumerate(job_links.items()):
-            print(f"Scrape link {i+1} / {len(job_links)}: {link}")
+            logging.info(f"Scrape link {i+1} / {len(job_links)}: {link}")
             job_element.click()
             time.sleep(randint(2, 5))
             soup = BeautifulSoup(self.driver.page_source, 'html.parser')
@@ -191,7 +200,7 @@ class SearchLinkedin:
                 data['Location'].append(location)
                 data['Description'].append(description)
             except Exception as e:
-                print(str(e))
+                logging.error(str(e))
         return data
 
     def find_jobs(self):
@@ -199,30 +208,30 @@ class SearchLinkedin:
         # Collect all jobs for each page
         page_num = 1
         while True:
-            print(f"Processing page {page_num}")
+            logging.info(f"Processing page {page_num}")
             try:
                 page_button = self.driver.find_element(By.XPATH, f'//button[@aria-label="Page {page_num}"]')
             except Exception as e:
-                print(str(e))
+                logging.error(str(e))
                 time.sleep(randint(3, 6))
                 break
             page_button.click()
             time.sleep(randint(3, 6))
 
-            print("Find all job links on the current page.")
-            job_links = self.find_page_jobs()
-
-            print("Extract job title, company, location and description.")
-            data = self.extract_data(job_links)
-            self.job_data['Link'].extend(data['Link'])
-            self.job_data['Title'].extend(data['Title'])
-            self.job_data['Company'].extend(data['Company'])
-            self.job_data['Location'].extend(data['Location'])
-            self.job_data['Description'].extend(data['Description'])
+            # logging.info("Find all job links on the current page.")
+            # job_links = self.find_page_jobs()
+            #
+            # logging.info("Extract job title, company, location and description.")
+            # data = self.extract_data(job_links)
+            # self.job_data['Link'].extend(data['Link'])
+            # self.job_data['Title'].extend(data['Title'])
+            # self.job_data['Company'].extend(data['Company'])
+            # self.job_data['Location'].extend(data['Location'])
+            # self.job_data['Description'].extend(data['Description'])
 
             page_num += 1
-            if page_num > 30:
-                print(f"Finished {page_num} job pages. Exit.")
+            if page_num > 2:
+                logging.info(f"Finished {page_num} job pages. Exit.")
                 break
 
     def save_results(self):
@@ -232,24 +241,50 @@ class SearchLinkedin:
         """
         job_df = pd.DataFrame(self.job_data)
         out_file = path.join('data', datetime.today().date().strftime('%Y-%m-%d') + '_all.csv')
-        print(f"Save all jobs to {out_file}")
+        logging.info(f"Save all jobs to {out_file}")
         job_df.to_csv(out_file, index=True)
 
     def close_session(self):
         """This function closes the actual session"""
-        print('End of the session, see you later!')
+        logging.info('End of the session, see you later!')
         self.driver.close()
 
     def run(self):
         """Apply to job offers"""
-        self.driver.maximize_window()
-        time.sleep(randint(1, 2))
+        self.init_webdriver()
         self.login_linkedin()
         self.job_search()
         self.filter()
         self.find_jobs()
         self.save_results()
         self.close_session()
+
+
+def config_log(log_file, level=logging.INFO) -> None:
+    """
+    Configure a log file.
+    @param log_file: path to a log file
+    @param level: choices can be logging.DEBUG, logging.INFO, ...
+    """
+    log_dir = path.dirname(log_file)
+    if not path.exists(log_dir):
+        makedirs(log_dir)
+
+    # Remove all handlers from the root logger
+    root_logger = logging.getLogger()
+    for handler in root_logger.handlers:
+        logging.root.removeHandler(handler)
+
+    # Create a new log file
+    str_format = '%(asctime)s - %(levelname)s - %(message)s'
+    logging.basicConfig(filename=log_file, level=level, format=str_format)
+
+    # Define a Handler which writes messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    console.setFormatter(logging.Formatter(str_format, "%Y-%m-%d %H:%M:%S"))
+    # add the handler to the root logger
+    logging.getLogger('').addHandler(console)
 
 
 def is_geoai_job(description):
@@ -265,8 +300,14 @@ def is_geoai_job(description):
 
 
 if __name__ == '__main__':
+
+    # configure a log file
+    config_log(path.join('data', 'logs', datetime.today().date().strftime('%Y-%m-%d') + '.log'))
+
+    logging.info(f"Load a configuration file for job search.")
     with open('data/config.json') as config_file:
         config = json.load(config_file)
 
     bot = SearchLinkedin(config)
     bot.run()
+    logging.info("All done!")
